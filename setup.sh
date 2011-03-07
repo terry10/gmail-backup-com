5469949478ebf91a1c3866184645afdbd1170d69
honza.svec <honza.svec@gmail.com>
2011-03-07 13:57:32 +0000

    Updated Linux release

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python2.5 -O /usr/lib/python2.5/py_compile.py gmail-backup-gui.py
-python2.5 -O /usr/lib/python2.5/py_compile.py gmail-backup.py
-python2.5 -O /usr/lib/python2.5/py_compile.py gmb.py
+#python2.5 -O /usr/lib/python2.5/py_compile.py gmail-backup-gui.py
+#python2.5 -O /usr/lib/python2.5/py_compile.py gmail-backup.py
+#python2.5 -O /usr/lib/python2.5/py_compile.py gmb.py
 mkdir dist_SH
-mv gmail-backup.pyo gmail-backup-gui.pyo gmb.pyo dist_SH
+cp gmail-backup.py gmail-backup-gui.py gmb.py dist_SH
 cp gmail-backup.sh gmail-backup-gui.sh gmb.gif gmb.ico dist_SH
 cp gmail-backup.pot dist_SH
 mkdir -p dist_SH/messages/cs_CZ/LC_MESSAGES
